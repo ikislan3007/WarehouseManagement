db5462759dec0c2d8f90f0c8ce795d3b1f043043
i.kislan <i.kislan@is-bg.net>
2022-12-15 17:11:49 +0200

    add controller

--- src/main/java/com/example/warehousemanagement/controller/AuthenticationController.java
+++ src/main/java/com/example/warehousemanagement/controller/AuthenticationController.java
@@ -1,7 +1,7 @@
 package com.example.warehousemanagement.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bnd.annotation.RestController;
 
 @RestController
 @RequestMapping("/authenticate-user")
